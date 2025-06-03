dbcffdf3531f9066e395bf9b9012ae72e61a9e7e
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-06-02 21:39:30 -0300

    finalizando

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 function buscarPokemon() {
+    console.log("buscarPokemon")
     const inputpokemon = document.getElementById("input_pokemon");
     const nomepokemon = inputpokemon.value;
     console.log("buscando pokemon " + nomepokemon);
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto" + nomepokemon)
+    fetch("https://pokeapi.co/api/v2/pokemon/" + nomepokemon)
       .then((resposta) => {
         return resposta.json();
     })
     .then((json) => {
       console.log(json);
-      const inputpokemon = document.getElementById("input_pokemon");
-      inputpokemon.value = json.Pokemon;
 
+      const altura = document.getElementById ("altura")
+      altura.innerText = "A altura é: " + json.height;
+
+      const peso = document.getElementById ("peso")
+      peso.innerHTML = "O peso é: " + json.weight;
+
+      const imagem = document.getElementById ("imagem")
+      imagem.src = json.sprites.other.dream_world.front_default;
     });
 }
 
 function configurarEventos() {
-    const inputpokemon = document.getElementById("input_pokemon");
+
+    const inputpokemon = document.getElementById("botao_buscar");
     inputpokemon.addEventListener("click", buscarPokemon);
   
    }
